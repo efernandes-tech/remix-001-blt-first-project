38acf19d3825b4dcca6ff27f5f6d820df18a181d
efernandes-tech <efernandes.tech@gmail.com>
2025-08-24 13:54:27 -0300

    Numeric value types

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.7;
 
 contract HelloWorld {
 
+    int8 public age = 34; // 8bits = between -128 and 128 - default int256 - other languages = 32
+
+    uint8 public age2 = 34; // between 0 and 255
+
+    // solidity don't have float/double
+
     string public message = "Hello World!";
 
 }

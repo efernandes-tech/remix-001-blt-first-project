8543c62228cbc22f0cc7a7bcf08f04f3f8b84717
efernandes-tech <efernandes.tech@gmail.com>
2025-08-24 14:06:44 -0300

    Access modifiers

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.7;
 
 contract HelloWorld {
 
-    int8 public age = 34; // 8bits = between -128 and 128 - default int256 - other languages = 32
+    int8 internal age = 34; // 8bits = between -128 and 128 - default int256 - other languages = 32
 
-    uint8 public age2 = 34; // between 0 and 255
+    uint8 private age2 = 34; // between 0 and 255
 
     // solidity don't have float/double
 

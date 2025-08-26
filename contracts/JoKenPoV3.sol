85958978d8a786fefaed1f35b862d5715ee2a57d
efernandes-tech <efernandes.tech@gmail.com>
2025-08-25 23:07:49 -0300

    Arrays in Solidity

---
+++ contracts/JoKenPoV3.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+contract JoKenPoV3 {
+
+    enum Options { NONE, ROCK, PAPER, SCISSORS } // 0, 1, 2, 3
+
+    Options private choice1 = Options.NONE;
+    address private player1;
+    string public result = "";
+
+    address payable private immutable owner;
+
+    address[] public players;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function exists(address winner) private view returns(bool) {
+        for(uint i=0; i < players.length; i++){
+            if(players[i] == winner){ 
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function finishGame(string memory newResult, address winner) private {
+        address contractAddress = address(this);
+        payable(winner).transfer((contractAddress.balance / 100) * 90);
+        owner.transfer(contractAddress.balance);
+
+        if (!exists(winner))
+            players.push(winner);
+
+        result = newResult;
+        player1 = address(0);
+        choice1 = Options.NONE;
+    }
+
+    function getBalance() public view returns(uint) {
+        require(owner == msg.sender, "You don't have this permission.");
+        return address(this).balance;
+    }
+
+    function play(Options newChoice) public payable {
+        require(newChoice != Options.NONE, "Invalid choice");
+        require(player1 != msg.sender, "Wait the another player.");
+        require(msg.value >= 0.01 ether, "Invalid bid.");
+
+        if(choice1 == Options.NONE){
+            player1 = msg.sender;
+            choice1 = newChoice;
+            result = "Player 1 choose his/her option. Waiting player 2.";
+        }
+        else if(choice1 == Options.ROCK && newChoice == Options.SCISSORS)
+            finishGame("Rock breaks scissors. Player 1 won.", player1);
+        else if(choice1 == Options.PAPER && newChoice == Options.ROCK)
+            finishGame("Paper wraps rock. Player 1 won.", player1);
+        else if(choice1 == Options.SCISSORS && newChoice == Options.PAPER)
+            finishGame("Scissors cuts paper. Player 1 won.", player1);
+        else if(choice1 == Options.SCISSORS && newChoice == Options.ROCK)
+            finishGame("Rock breaks scissors. Player 2 won.", msg.sender);
+        else if(choice1 == Options.ROCK && newChoice == Options.PAPER)
+            finishGame("Paper wraps rock. Player 2 won.", msg.sender);
+        else if(choice1 == Options.PAPER && newChoice == Options.SCISSORS)
+            finishGame("Scissors cuts paper. Player 2 won.", msg.sender);
+        else {
+            result = "Draw game. The prize was doubled.";
+            player1 = address(0);
+            choice1 = Options.NONE;
+        }
+    }
+}

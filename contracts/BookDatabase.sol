57a3dfba1ce0509ac6df8fffb090498f49cb4f16
efernandes-tech <efernandes.tech@gmail.com>
2025-08-26 23:25:20 -0300

    Function modifiers

--- contracts/BookDatabase.sol
+++ contracts/BookDatabase.sol
@@ -12,6 +12,12 @@ contract BookDatabase {
     uint32 private nextId = 0;
     mapping(uint32 => Book) public books;
 
+    address private immutable owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function addBook(Book memory newBook) public {
         nextId++;
         books[nextId] = newBook;
@@ -37,8 +43,13 @@ contract BookDatabase {
             books[id].year = newBook.year;
     }
 
-    function removeBook(uint32 id) public {
+    function removeBook(uint32 id) public restricted {
         delete books[id];
+    }
+
+    modifier restricted() {
+        require(owner == msg.sender, "You don't have permission.");
+        _;
     }
 
 }
